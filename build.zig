7bdb65b7d8b217e9efb086d88115311b185f2670
drzln <drzzln@protonmail.com>
2025-05-11 03:42:06 -0300

    init

--- build.zig
+++ build.zig
@@ -7,13 +7,10 @@ const t = @import("build/tests.zig");
 pub fn build(b: *std.Build) void {
     const target = opts.target(b);
     const optimize = opts.optimize(b);
-
     const yaml_pkg = deps.yaml(b);
     const cfg_mod = deps.configModule(b, yaml_pkg);
-
     art.makeLibrary(b, target, optimize, cfg_mod, yaml_pkg);
     const exe = art.makeExecutable(b, target, optimize, cfg_mod, yaml_pkg);
     art.makeRunStep(b, exe);
-
     t.addAllTests(b, target, optimize, cfg_mod, yaml_pkg);
 }
